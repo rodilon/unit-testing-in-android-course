e552696c2a702c5e97f1ae2f2884b573ec84dbe8
rodilon <renanodilon@gmail.com>
2022-07-01 14:17:52 -0300

    segundo exercicio

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,38 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
+
+    StringDuplicator SUT;
+
+    @Before
+    public void setUp() {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyChar_returnEmptyChar() {
+        String result = SUT.duplicate("");
+
+        assertEquals(result, "");
+    }
+
+    @Test
+    public void duplicate_singleChar_returnSingleCharDuplicated() {
+        String result = SUT.duplicate("a");
+
+        assertEquals(result, "aa");
+    }
+
+    @Test
+    public void duplicate_longChar_returnLongCharDuplicated() {
+        String result = SUT.duplicate("Renan");
+
+        assertEquals(result, "RenanRenan");
+    }
 
 }
