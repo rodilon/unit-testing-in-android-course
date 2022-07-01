f330c1abd1e6b9b8fdb0ecdc490af6806e894669
rodilon <renanodilon@gmail.com>
2022-07-01 13:01:47 -0300

    primeiro exercicio

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
 
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test() {
+        boolean result = SUT.isNegative(-1);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void teste1() {
+        boolean result = SUT.isNegative(0);
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void teste2() {
+        boolean result = SUT.isNegative(1);
+
+        Assert.assertFalse(result);
+    }
 }
